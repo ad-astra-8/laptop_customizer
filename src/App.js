1812603133b7f6cf49caf2ea84fbb309a6508802
Wanda Antoine <wanda@thinkful.com>
2020-04-25 11:20:32 -0400

    created new components files

--- src/App.js
+++ src/App.js
@@ -122,3 +122,64 @@ class App extends Component {
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+
+// // Normalizes string as a slug - a string that is safe to use
+// // in both URLs and html attributes
+// import slugify from 'slugify';
+// import './App.css';
+// import CustomizeLaptop from './src/CustomizeLaptop';
+// import Cart from './src/Cart';
+
+
+// // This object will allow us to
+// // easily convert numbers into US dollar values
+// const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+//     style: 'currency',
+//     currency: 'USD'
+//   });
+
+  
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             selected: {
+//                 Processor: {
+//                     name: '17th Generation Intel Core HB (7 Core with donut spare)',
+//                     cost: 700
+//                 },
+//                 'Operating System': {
+//                     name: 'Ubuntu Linux 16.04',
+//                     cost: 200
+//                 },
+//                 'Video Card': {
+//                     name: 'Toyota Corolla 1.5v',
+//                     cost: 1150.98
+//                 },
+//                 Display: {
+//                     name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+//                     cost: 1500
+//                 }
+//             }
+//         };
+//     }
+//     render() {
+//         return (
+//             <div className="App">
+//                 <header>
+//                     <h1>ELF Computing | Laptops</h1>
+//                 </header>
+//                 <CustomizeLaptop
+//                     features={this.props.features}
+//                     selected={this.state.selected} />
+//                 <Cart selected={this.state.selected} />
+
+
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
